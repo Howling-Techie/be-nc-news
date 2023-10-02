e62bfd610caafaafa14d4e26b6b17e1cccf04466
Matt B <howling.techie@gmail.com>
2023-10-02 15:42:03 +0100

    Corrected status code

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -3,7 +3,7 @@ const {deleteComment} = require("../models/comments.model");
 exports.deleteComment = (req, res, next) => {
   deleteComment(req.params.comment_id)
     .then(() => {
-      res.status(201).send();
+      res.status(204).send();
     })
     .catch((error) => {
       next(error);
