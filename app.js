8d003ab5b5f913b8029a2a0f867997f98bbcfb2b
Matt B <howling.techie@gmail.com>
2023-10-04 16:45:28 +0100

    Corrected error in filepath.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const app = express();
 const apiRouter = require("./routes/api.router");
+const path = require("path");
 app.use(express.json());
 
 app.use("/api", apiRouter);
 app.get("/", (req, res) => {
-  res.sendFile("./api.html");
+  const filePath = path.join(__dirname, "api.html");
+  res.sendFile(filePath);
 });
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
