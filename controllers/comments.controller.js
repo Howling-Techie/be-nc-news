911149d3392408349302596c025f18b43946ac0c
Matt B <howling.techie@gmail.com>
2023-10-04 10:26:21 +0100

    Added endpoint to submit votes to a comment

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,9 +1,19 @@
-const {deleteComment} = require("../models/comments.model");
+const {deleteComment, updateComment} = require("../models/comments.model");
 
 exports.deleteComment = (req, res, next) => {
   deleteComment(req.params.comment_id)
     .then(() => {
       res.status(204).send();
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+exports.patchComment = (req, res, next) => {
+  updateComment(req.params.comment_id, req.body)
+    .then((comment) => {
+      res.status(200).send({comment});
     })
     .catch((error) => {
       next(error);
