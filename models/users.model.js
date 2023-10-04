3231e9fe774386ac97349d5defc62750a094075d
Matt B <howling.techie@gmail.com>
2023-10-04 10:10:01 +0100

    Added endpoint to fetch specific user

--- models/users.model.js
+++ models/users.model.js
@@ -4,3 +4,14 @@ exports.selectUsers = async () => {
   return (await db.query(`SELECT *
                           FROM users`)).rows;
 };
+
+exports.selectUser = async (username) => {
+  const results = await db.query(`SELECT *
+                                  FROM users
+                                  WHERE username = $1`, [username]);
+
+  if (results.rows.length === 0) {
+    return Promise.reject({status: 404, msg: "User not found"});
+  }
+  return results.rows[0];
+};
