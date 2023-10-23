a42c477d7bbdb2cbb2a8ee2d873d4fa97a014a59
Howling Techie <howling.techie@gmail.com>
2023-10-23 09:32:45 +0100

    allow cross-origin resource sharing

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api.router");
 const path = require("path");
+const cors = require('cors');
+
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", apiRouter);
 app.get("/", (req, res) => {
